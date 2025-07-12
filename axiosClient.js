cd54391c8449341c1da74997ccfd4a3405154c86
ajaywaldia <42498395+ajaywaldia@users.noreply.github.com>
2025-07-12 18:02:43 +0530

    Update axiosClient.js

--- axiosClient.js
+++ axiosClient.js
@@ -1,1 +1,18 @@
 
+function getAxiosInstanceWithTimeoutConfig(logger) {
+  const instance = axios.create({
+    timeout: process.env.AXIOS_TIMEOUT_MS || 5000,
+  });
+
+  instance.interceptors.response.use(
+    (response) => {
+      if (logger) logger.info({ url: response.config.url }, 'Axios response received');
+      return response;
+    },
+    (error) => {
+      if (logger) logger.error({ err: error.message }, 'Axios request failed');
+      return Promise.reject(error);
+    }
+  );
+
+module.export = getAxiosInstanceWithTimeoutConfig;
