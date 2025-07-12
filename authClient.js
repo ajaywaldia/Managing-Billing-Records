eb3cc835aacf9114dbd058631fd1239bee8fd798
ajaywaldia <42498395+ajaywaldia@users.noreply.github.com>
2025-07-12 18:28:29 +0530

    Update authClient.js

--- authClient.js
+++ authClient.js
@@ -1,1 +1,11 @@
 
+//  implementation — of the code which checks the authentication based on the project details could be resource group details based on some logic Azure Active Directry can come into picture here
+async function Get(projectId) {
+  // Generate an authorization tolen 
+  return {
+    accessToken: 'Some-token',
+    projectId: projectId,
+  };
+}
+
+module.exports = Get;
